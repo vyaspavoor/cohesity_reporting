bbc7b898fe325632c77ff372e1324770a193bda8
unknown <Administrator@fcci.sre.cohesity.com>
2021-05-11 14:38:31 +0100

    commit

--- powershell/registerSource/registerNASSourceSMB.ps1
+++ powershell/registerSource/registerNASSourceSMB.ps1
@@ -8,15 +8,25 @@ param (
     [String]
     $NASList
 )
-
+$ErrorActionPreference = "Stop";
 try{
-    #Connect-CohesityCluster -Server $ClusterFQDN -Credential (get-credential -t 'Please enter the Cluster')
-    Import-CSV | ForEach-Object{
-        $NASHostName = $_.'Hostname'
-        $NASPath = $_.'Path'
-        Write-Output $NASHostName + $nasPath
-        #Register-CohesityProtectionSourceSMB
+    $cred = Get-Credential -Message "enter this"
+    Connect-CohesityCluster -Server $ClusterFQDN -Credential $cred
+}
+catch{Write-Output "Could not connect to the Cohesity Cluster.  Please make sure that the cluster FQDN or VIP is correct"}
+$ValidPath = Test-Path $NASList
+try{  
+    if ($ValidPath -eq $True){
+        Import-CSV $NASList | ForEach-Object{
+            $NASHostName = $_.'Hostname'
+            $NASPath = $_.'Path'
+            $Path ='\\'+ $NasHostName + '\' + $NASHostName
+            Write-Output $Path
+    #Register-CohesityProtectionSourceSMB
+        }
+    }
+    else {
+        Write-Output "The file $NASList is not a valid file"
     }    
 }
 catch{}
-Connect-CohesityCluster -Server 
