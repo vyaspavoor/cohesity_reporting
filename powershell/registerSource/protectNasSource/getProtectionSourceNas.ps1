ef7517509555f8d42eb38e7185ab9eb73ad28d9b
greysave <42587531+greysave@users.noreply.github.com>
2021-05-17 16:03:29 -0400

    Remove old file

--- powershell/registerSource/protectNasSource/getProtectionSourceNas.ps1
+++ powershell/registerSource/protectNasSource/getProtectionSourceNas.ps1
@@ -3,21 +3,14 @@
 param (
     [Parameter(Mandatory=$True, HelpMessage = "Please enter the cluster ID")]
     [String]
-    $ClusterFQDN,
-    
-    [Parameter(Manadory =$True)]
-    [ParameterType]
-    $exportedFile
+    $ClusterFQDN
 )
 $ErrorActionPreference = "Stop";
 $credcoh = Get-Credential -Message "Enter the Cohesity cluster credentials. "
-#$credSMB = Get-Credential -M "Please enter domain admin credentials with access to the SMB Shares"
-#$ValidPath = Test-Path $NASList
 try{Connect-CohesityCluster -Server $ClusterFQDN -Credential $credcoh}
 catch{Write-Output "Could not connect to the Cohesity Cluster.  Please make sure that the cluster FQDN or VIP is correct"}
-#Testing
 try{
     Get-CohesityProtectionSource -Environments KGenericNas
     Get-CohesityProtectionSource -Environments KGenericNas | export-csv GenericNasSources.csv -Delimiter ';'
 }
-catch{Write-Out "There was a problem importing Protection Sources"}
+catch{Write-Out "There was a problem exporting NAS Protection Sources"}
