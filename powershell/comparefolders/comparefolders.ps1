c7660219b34c86d95a827240ffd39ae9d7ee003e
greysave <42587531+greysave@users.noreply.github.com>
2021-06-11 19:19:13 -0400

    Compare File Final

--- powershell/comparefolders/comparefolders.ps1
+++ powershell/comparefolders/comparefolders.ps1
@@ -7,13 +7,16 @@ param (
     [String]
     $target
 )
-
+#$ErrorActionPreference = Continue;
 $FileName = (Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")
 $LogFile = New-Item -itemType File -Name ("filehashcompare-" + $FileName + ".log")
 try{
-$sourceCompare = Get-ChildItem -Path $source -Recurse | Get-FileHash -Algorithm md5
-$targetCompare = Get-ChildItem -Path $target -Recurse  | Get-FileHash -Algorithm md5
+$sourceCompare = Get-ChildItem -Path $source -Recurse | Select-Object -Property *,
+@{name="Hash";expression={(Get-FileHash $_.FullName -Algorithm MD5).hash}}
+$targetCompare = Get-ChildItem -Path $target -Recurse | Select-Object -Property *,
+@{name="Hash";expression={(Get-FileHash $_.FullName -Algorithm MD5).hash}}
+
 }
 catch{Write-warning $_.exception.message}
-try{Compare-Object -ReferenceObject $sourceCompare -DifferenceObject $targetCompare | Tee-Object -file $LogFile -Append}
+try{Compare-Object -ReferenceObject $sourceCompare -DifferenceObject $targetCompare -Property Name,Hash | Tee-Object -file $LogFile -Append}
 catch{Write-warning $_.exception.message}
