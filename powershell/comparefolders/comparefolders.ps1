83a25da22229da78e2e4dd7c7d73daa8902dbd9f
greysave <42587531+greysave@users.noreply.github.com>
2021-06-11 16:52:16 -0400

    Added CompareFolders

--- powershell/comparefolders/comparefolders.ps1
+++ powershell/comparefolders/comparefolders.ps1
@@ -7,11 +7,13 @@ param (
     [String]
     $target
 )
+
+$FileName = (Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")
+$LogFile = New-Item -itemType File -Name ("filehashcompare-" + $FileName + ".log")
 try{
-$sourceCompare = Get-ChildItem -Recurse -Path $source
-$targetCompare = Get-ChildItem -Recurse -Path $target
+$sourceCompare = Get-ChildItem -Path $source -Recurse | Get-FileHash -Algorithm md5
+$targetCompare = Get-ChildItem -Path $target -Recurse  | Get-FileHash -Algorithm md5
 }
 catch{Write-warning $_.exception.message}
-
-try{Compare-Object -ReferenceObject $sourceCompare -DifferenceObject $targetCompare}
+try{Compare-Object -ReferenceObject $sourceCompare -DifferenceObject $targetCompare | Tee-Object -file $LogFile -Append}
 catch{Write-warning $_.exception.message}
