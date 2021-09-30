a4d4ea5688f251584bdf188a6325a6b9bb63214a
Gregory Savage <gregory.savage@cohesity.com>
2021-09-29 23:57:37 -0400

    Updated encrypt all and new file

--- powershell/encryptAll/encrytAll.ps1
+++ powershell/encryptAll/encrytAll.ps1
@@ -1,1 +1,4 @@
-gci -re  *.*  |% { Protect-File -KeyAsPlainText $key $_ }
+$vault = "c:\Users\Administrator\Documents\Crypt\Vault1*"
+$key= (get-content -path "c:\Users\Administrator\Documents\Keys\cryptkey1.txt" -Raw)
+
+get-childitem -path $vault -recurse | % { Protect-File -KeyAsPlainText $key $_ }
