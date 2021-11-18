a02e873c47bc59f6a6d8fcabe87601d12bc01ed6
greysave <42587531+greysave@users.noreply.github.com>
2021-11-18 00:27:18 -0500

    protection policies and protection runs

--- Python/Custom_Reporting/Custom_Reporting.py
+++ Python/Custom_Reporting/Custom_Reporting.py
@@ -29,7 +29,12 @@ class CohesityUserAuthentication(object):
     #Authenticate to the cluster with credentials
 
     def user_auth(self):
-        return CohesityClient(self.cluster_url, self.username, self.password, self.domain)
+        try:
+            return CohesityClient(self.cluster_url, self.username, self.password, self.domain)
+        except RequestErrorErrorException as e: 
+            print("There was an excepiton with the JSON Payload")
+        except APIException as e:
+            print("There was a problem with the your credentials  Please try again", e)
     
     def get_bearer_token(self):
         self.access_token = self.cohesity_client.access_tokens
@@ -55,22 +60,28 @@ class CohesityProtectionJobObject(object):
         
         #Declare protection jobs object
         self.protection_jobs = cohesity_client.protection_jobs
-        self.protection_job_objects = self.protection_jobs.get_protection_jobs()
+        try:
+            self.protection_job_objects = self.protection_jobs.get_protection_jobs()
+        except RequestErrorErrorException as e: 
+            print("There was a problem with the payload")
+        except APIException as e:
+            print("There was a problem with the API request")
         
         #Create policy variables and objects
         self.protection_policies = cohesity_client.protection_policies
         
-        
         #Create source object
         self.protection_sources = cohesity_client.protection_sources
+        
+         #Create job run object
+        self.protection_runs = cohesity_client.protection_runs
         
         #Declare empty dictionary
         self.job_dict = {}
         
         #Declare job_name list
         self.job_names = []
-       
- 
+        
     
     def create_job_dictionary(self):
         for job in self.protection_job_objects:
@@ -87,12 +98,10 @@ class CohesityProtectionJobObject(object):
             self.job_names.append(name)
         return self.job_names
         
-
     def append_policy_name_dictionary(self, appended_job_dict, job_name_list):
         count = 0
         #self.protection_policy_objects = self.protection_policies.get_protection_policies()
         self.appended_job_dict = appended_job_dict
-        
         for name in job_name_list:
     
             try:
@@ -102,7 +111,7 @@ class CohesityProtectionJobObject(object):
             except APIException as e:
                 continue    
             self.appended_job_dict[name]["Policy Name"] = protection_policy_objects.name
-            return self.appended_job_dict
+        return self.appended_job_dict
    
             
 
@@ -122,7 +131,45 @@ class CohesityProtectionJobObject(object):
                 source_names.append(source[0].protection_source.name)
             self.appended_job_dict[name]["Source Name"] = source_names
         return self.appended_job_dict
-                #print(dir(source[0]))
+                
+                
+    def append_latest_protection_run_dictionary(self, appended_job_dict, job_names_list):
+        self.appended_job_dict = appended_job_dict
+        
+        for name in job_names_list:
+            run_id = []
+            try:
+                job_run = self.protection_runs.get_protection_runs(job_id = self.appended_job_dict[name]["Job Id"], num_runs= 2)
+            except RequestErrorErrorException as e: 
+                continue
+            except APIException as e:
+                continue
+            
+            for job in job_run:
+                
+                # print(self.appended_job_dict[name])
+                if self.appended_job_dict[name]["Job Id"] == job.job_id:
+                    run_id.append(job.backup_run.job_run_id)
+                self.appended_job_dict[name]["Run Ids"] = run_id
+        return self.appended_job_dict
+    
+    
+            
+            # print(run_id)
+                # print(job.job_name, job.backup_run.job_run_id)
+            # for job_id in self.appended_job_dict[name]["Job Id"]:
+                # try:
+                #     job_run = self.protection_runs.get_protection_runs(job_id = job_id["Job Id"])
+                # except RequestErrorErrorException as e: 
+                #     continue
+                # except APIException as e:
+                #     continue
+            # print(job_run.backup_run.start_time_usecs)
+            # print(self.appended_job_dict[name]["Job Id"])
+                #print(job_id[name])
+        
+    
+
 def main():
     #SDK Authenticaation to the cluster
     cohesity_client = CohesityUserAuthentication()
@@ -134,9 +181,9 @@ def main():
     protection_job_dictionary = protection_job_object.create_job_dictionary()
     cohesity_protection_jobs_names = protection_job_object.create_job_name_list(protection_job_dictionary)
     cohesity_protection_policy_names = protection_job_object.append_policy_name_dictionary(protection_job_dictionary, cohesity_protection_jobs_names)
-    cohesity_protection_source_names = protection_job_object.append_source_name_dictionary(protection_job_dictionary, cohesity_protection_policy_names)
-    #prcohesity_protection_policy_names)
-    print(cohesity_protection_source_names)
+    cohesity_protection_source_names = protection_job_object.append_source_name_dictionary(cohesity_protection_policy_names, cohesity_protection_jobs_names)
+    cohesity_protection_run_info = protection_job_object.append_latest_protection_run_dictionary(cohesity_protection_source_names, cohesity_protection_jobs_names)
+    #print(cohesity_protection_source_names)
     
     
     
